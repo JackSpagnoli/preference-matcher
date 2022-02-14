327760d8d3727ec364b34fea53eed23abc5c3f29
AmaanIbnNasar <amaan.ibn-nasar1@nhs.net>
2022-02-14 17:16:13 +0000

    Refactored file to make more sense and added tests

--- PreferenceMatcher.py
+++ PreferenceMatcher.py
@@ -1,14 +1,80 @@
 import json
+import pprint as pp
+import networkx as nx
+import itertools
+import matplotlib.pyplot as plt
 
 
 class PreferenceMatcher():
-  
-  def readPreferenceFile(self):
-    preferenceFile = open("preferences.json", "r")
-    preferences = json.load(preferenceFile)
-    preferenceFile.close()
-    return preferences
-  
-  
+
+    def readPreferenceFile(self):
+        preferenceFile = open("preferences.json", "r")
+        preferences = json.load(preferenceFile)
+        preferenceFile.close()
+        return preferences
+
+    def extractPlacementNames(self, placements):
+        placementNames = []
+        for k in placements:
+            if placements[k]["numberOfGrads"] > 1:
+                numberedNames = []
+                for i in range(1, placements[k]["numberOfGrads"]+1):
+                    numberedNames.append(f"{k}_{i}")
+                placementNames.extend(numberedNames)
+            else:
+                placementNames.append(k)
+        return placementNames
+
+    def extractPeopleNames(self, preferences):
+        return [k for k in preferences]
+
+    def convertPreferencesToGraph(self, preferences):
+        preferencesGraph = nx.Graph()
+        self.placementNames = self.extractPlacementNames(
+            preferences["placements"])
+        self.peopleNames = self.peopleNames = self.extractPeopleNames(
+            preferences["preferences"])
+        preferencesGraph.add_nodes_from(self.placementNames)
+        preferencesGraph.add_nodes_from(self.peopleNames)
+        preferencesGraph.add_edges_from(
+            list(itertools.product(self.peopleNames, self.placementNames)), weight=100)
+        return preferencesGraph
+
+    def drawGraph(self, graph):
+        nx.draw(graph, with_labels=True)
+        pos = nx.spring_layout(graph)
+        labels = {e: graph.edges[e]["weight"] for e in graph.edges}
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+        plt.show()
+
+    def applyPreferenceWeighting(self, graph, preferences):
+        for person in self.peopleNames:
+            firstPreference = preferences["preferences"][person]["firstPreference"]
+            secondPreference = preferences["preferences"][person]["secondPreference"]
+            thirdPreference = preferences["preferences"][person]["thirdPreference"]
+            if preferences["placements"][firstPreference]["numberOfGrads"] > 1:
+                for i in range(1, preferences["placements"][firstPreference]["numberOfGrads"]+1):
+                    graph[person][f"{firstPreference}_{i}"]["weight"] += 75
+            else:
+                graph[person][firstPreference]["weight"] += 75
+            if preferences["placements"][secondPreference]["numberOfGrads"] > 1:
+                for i in range(1, preferences["placements"][secondPreference]["numberOfGrads"]+1):
+                    graph[person][f"{secondPreference}_{i}"]["weight"] += 50
+            else:
+                graph[person][secondPreference]["weight"] += 50
+            if preferences["placements"][thirdPreference]["numberOfGrads"] > 1:
+                for i in range(1, preferences["placements"][thirdPreference]["numberOfGrads"]+1):
+                    graph[person][f"{thirdPreference}_{i}"]["weight"] += 25
+            else:
+                graph[person][thirdPreference]["weight"] += 25
+
+
 if __name__ == "__main__":
-  pass
+    prefMatcher = PreferenceMatcher()
+    preferences = prefMatcher.readPreferenceFile()
+    preferenceGraph = prefMatcher.convertPreferencesToGraph(preferences)
+    prefMatcher.drawGraph(preferenceGraph)
+    prefMatcher.applyPreferenceWeighting(preferenceGraph, preferences)
+    prefMatcher.drawGraph(preferenceGraph)
+    pp.pprint(nx.maximal_matching(preferenceGraph))
+    pp.pprint(nx.max_weight_matching(preferenceGraph))
--- PreferenceMatcher_test.py
+++ PreferenceMatcher_test.py
@@ -1,196 +1,267 @@
 import unittest
-
+import networkx as nx
 from PreferenceMatcher import PreferenceMatcher
 
-preferencesObj = {
-      "placements": [
-          {
-            "directorate": "Data Services",
-            "name": "Adult Social Care Statistics",
-            "numberOfGrads": 1
-          },
-          {
-            "directorate": "Data Services",
-            "name": "Business Intelligence/Data Visualisation",
-            "numberOfGrads": 1
-          },
-          {
-            "directorate": "Data Services",
-            "name": "Data Management/Engineering",
-            "numberOfGrads": 2
-          },
-          {
-            "directorate": "Data Services",
-            "name": "Turing Tribe",
-            "numberOfGrads": 1
-          },
-          {
-            "directorate": "Platforms",
-            "name": "Data Quality Dashboards",
-            "numberOfGrads": 2
-          },
-          {
-            "directorate": "Platforms",
-            "name": "Spine Core",
-            "numberOfGrads": 3
-          },
-          {
-            "directorate": "Product Development",
-            "name": "NHS Pathways - Development Team",
-            "numberOfGrads": 1
-          },
-          {
-            "directorate": "Product Development",
-            "name": "NHS Pathways - Reporting Team",
-            "numberOfGrads": 1
-          },
-          {
-            "directorate": "Product Development",
-            "name": "111 Online - Developer",
-            "numberOfGrads": 1
-          },
-          {
-            "directorate": "IT Operations (Including Cyber Security)",
-            "name": "Cyber Security",
-            "numberOfGrads": 1
-          },
-          {
-            "directorate": "IT Operations (Including Cyber Security)",
-            "name": "Core Infrastructure Services - Networks",
-            "numberOfGrads": 1
-          },
-          {
-            "directorate": "IT Operations (Including Cyber Security)",
-            "name": "Core Infrastructure Services - Sustainable Hybrid Cloud",
-            "numberOfGrads": 1
-          },
-          {
-            "directorate": "IT Operations (Including Cyber Security)",
-            "name": "Live Services - IT Operations Centre",
-            "numberOfGrads": 1
-          }
-        ],
-      "preferences": [
-        {
-          "name": "Amaan Ibn-Nasar",
-          "firstPreference": "Spine Core",
-          "secondPreference": "Adult Social Care Statistics",
-          "thirdPreference": "111 Online - Developer"
-        },
-        {
-          "name": "Abbie Prescott",
-          "firstPreference": "Live Services - IT Operations Centre",
-          "secondPreference": "Cyber Security",
-          "thirdPreference": "Data Quality Dashboards"
-        },
-        {
-          "name": "Adam Carruthers",
-          "firstPreference": "Core Infrastructure Services - Networks",
-          "secondPreference": "Spine Core",
-          "thirdPreference": "111 Online - Developer"
-        },
-        {
-          "name": "Alice Tapper",
-          "firstPreference": "Business Intelligence/Data Visualisation",
-          "secondPreference": "Cyber Security",
-          "thirdPreference": "Turing Tribe"
-        },
-        {
-          "name": "Amelia Noonan",
-          "firstPreference": "Data Quality Dashboards",
-          "secondPreference": "Business Intelligence/Data Visualisation",
-          "thirdPreference": "Cyber Security"
-        },
-        {
-          "name": "Anna Evans",
-          "firstPreference": "Cyber Security",
-          "secondPreference": "Turing Tribe",
-          "thirdPreference": "NHS Pathways - Development Team"
-        },
-        {
-          "name": "Benjamin Wallace",
-          "firstPreference": "Adult Social Care Statistics",
-          "secondPreference": "111 Online - Developer",
-          "thirdPreference": "Spine Core"
-        },
-        {
-          "name": "Joel Helbling",
-          "firstPreference": "NHS Pathways - Development Team",
-          "secondPreference": "Cyber Security",
-          "thirdPreference": "Turing Tribe"
-        },
-        {
-          "name": "Joseph Wilson",
-          "firstPreference": "Spine Core",
-          "secondPreference": "Data Quality Dashboards",
-          "thirdPreference": "Core Infrastructure Services - Networks"
-        },
-        {
-          "name": "Laura Thirft",
-          "firstPreference": "Data Management/Engineering",
-          "secondPreference": "NHS Pathways - Reporting Team",
-          "thirdPreference": "Core Infrastructure Services - Networks"
-        },
-        {
-          "name": "Maisie Blyth",
-          "firstPreference": "Spine Core",
-          "secondPreference": "Turing Tribe",
-          "thirdPreference": "Core Infrastructure Services - Sustainable Hybrid Cloud"
-        },
-        {
-          "name": "Mitul Dattani",
-          "firstPreference": "Core Infrastructure Services - Sustainable Hybrid Cloud",
-          "secondPreference": "Data Quality Dashboards",
-          "thirdPreference": "Spine Core"
-        },
-        {
-          "name": "Nathan Gregory",
-          "firstPreference": "Live Services - IT Operations Centre",
-          "secondPreference": "Data Management/Engineering",
-          "thirdPreference": "Cyber Security"
-        },
-        {
-          "name": "Nathan Pettit",
-          "firstPreference": "111 Online - Developer",
-          "secondPreference": "Spine Core",
-          "thirdPreference": "Turing Tribe"
-        },
-        {
-          "name": "Oluwadamiloju Makinde",
-          "firstPreference": "Data Quality Dashboards",
-          "secondPreference": "Business Intelligence/Data Visualisation",
-          "thirdPreference": "Turing Tribe"
-        },
-        {
-          "name": "Roshaan Bajwa",
-          "firstPreference": "Turing Tribe",
-          "secondPreference": "Spine Core",
-          "thirdPreference": "Cyber Security"
-        },
-        {
-          "name": "Scott Caldwell-Nichols",
-          "firstPreference": "Data Management/Engineering",
-          "secondPreference": "NHS Pathways - Reporting Team",
-          "thirdPreference": "Spine Core"
-        },
-        {
-          "name": "Zahra Ahmed",
-          "firstPreference": "NHS Pathways - Reporting Team",
-          "secondPreference": "111 Online - Developer",
-          "thirdPreference": "Data Management/Engineering"
-        }
-      ]
-    }
 
 class PreferenceMatcherTest(unittest.TestCase):
 
-  def setUp(self) -> None:
-    self.preferenceMatcher = PreferenceMatcher()
-  
-  
-  def test_readPreferenceFile(self):
-    self.assertEqual(preferencesObj, self.preferenceMatcher.readPreferenceFile())
-    
+    def setUp(self) -> None:
+        self.preferenceMatcher = PreferenceMatcher()
+        self.preferencesObj = {
+            "placements": {
+                "Adult Social Care Statistics": {
+                    "directorate": "Data Services",
+                    "numberOfGrads": 1
+                },
+                "Business Intelligence/Data Visualisation": {
+                    "directorate": "Data Services",
+                    "numberOfGrads": 1
+                },
+                "Data Management/Engineering": {
+                    "directorate": "Data Services",
+                    "numberOfGrads": 2
+                },
+                "Turing Tribe": {
+                    "directorate": "Data Services",
+                    "numberOfGrads": 1
+                },
+                "Data Quality Dashboards": {
+                    "directorate": "Platforms",
+                    "numberOfGrads": 2
+                },
+                "Spine Core": {
+                    "directorate": "Platforms",
+                    "numberOfGrads": 3
+                },
+                "NHS Pathways - Development Team": {
+                    "directorate": "Product Development",
+                    "numberOfGrads": 1
+                },
+                "NHS Pathways - Reporting Team": {
+                    "directorate": "Product Development",
+                    "numberOfGrads": 1
+                },
+                "111 Online - Developer": {
+                    "directorate": "Product Development",
+                    "numberOfGrads": 1
+                },
+                "Cyber Security": {
+                    "directorate": "IT Operations (Including Cyber Security)",
+                    "numberOfGrads": 1
+                },
+                "Core Infrastructure Services - Networks": {
+                    "directorate": "IT Operations (Including Cyber Security)",
+                    "numberOfGrads": 1
+                },
+                "Core Infrastructure Services - Sustainable Hybrid Cloud": {
+                    "directorate": "IT Operations (Including Cyber Security)",
+                    "numberOfGrads": 1
+                },
+                "Live Services - IT Operations Centre": {
+                    "directorate": "IT Operations (Including Cyber Security)",
+                    "numberOfGrads": 1
+                }
+            },
+            "preferences": {
+                "Amaan Ibn-Nasar": {
+                    "firstPreference": "Spine Core",
+                    "secondPreference": "Adult Social Care Statistics",
+                    "thirdPreference": "111 Online - Developer"
+                },
+                "Abbie Prescott": {
+                    "firstPreference": "Live Services - IT Operations Centre",
+                    "secondPreference": "Cyber Security",
+                    "thirdPreference": "Data Quality Dashboards"
+                },
+                "Adam Carruthers": {
+                    "firstPreference": "Core Infrastructure Services - Networks",
+                    "secondPreference": "Spine Core",
+                    "thirdPreference": "111 Online - Developer"
+                },
+                "Alice Tapper": {
+                    "firstPreference": "Business Intelligence/Data Visualisation",
+                    "secondPreference": "Cyber Security",
+                    "thirdPreference": "Turing Tribe"
+                },
+                "Amelia Noonan":
+                {
+                    "firstPreference": "Data Quality Dashboards",
+                    "secondPreference": "Business Intelligence/Data Visualisation",
+                    "thirdPreference": "Cyber Security"
+                },
+                "Anna Evans":
+                {
+                    "firstPreference": "Cyber Security",
+                    "secondPreference": "Turing Tribe",
+                    "thirdPreference": "NHS Pathways - Development Team"
+                },
+                "Benjamin Wallace":
+                {
+                    "firstPreference": "Adult Social Care Statistics",
+                    "secondPreference": "111 Online - Developer",
+                    "thirdPreference": "Spine Core"
+                },
+                "Joel Helbling":
+                {
+                    "firstPreference": "NHS Pathways - Development Team",
+                    "secondPreference": "Cyber Security",
+                    "thirdPreference": "Turing Tribe"
+                },
+                "Joseph Wilson":
+                {
+                    "firstPreference": "Spine Core",
+                    "secondPreference": "Data Quality Dashboards",
+                    "thirdPreference": "Core Infrastructure Services - Networks"
+                },
+                "Laura Thirft":
+                {
+                    "firstPreference": "Data Management/Engineering",
+                    "secondPreference": "NHS Pathways - Reporting Team",
+                    "thirdPreference": "Core Infrastructure Services - Networks"
+                },
+                "Maisie Blyth":
+                {
+                    "firstPreference": "Spine Core",
+                    "secondPreference": "Turing Tribe",
+                    "thirdPreference": "Core Infrastructure Services - Sustainable Hybrid Cloud"
+                },
+                "Mitul Dattani":
+                {
+                    "firstPreference": "Core Infrastructure Services - Sustainable Hybrid Cloud",
+                    "secondPreference": "Data Quality Dashboards",
+                    "thirdPreference": "Spine Core"
+                },
+                "Nathan Gregory":
+                {
+                    "firstPreference": "Live Services - IT Operations Centre",
+                    "secondPreference": "Data Management/Engineering",
+                    "thirdPreference": "Cyber Security"
+                },
+                "Nathan Pettit":
+                {
+                    "firstPreference": "111 Online - Developer",
+                    "secondPreference": "Spine Core",
+                    "thirdPreference": "Turing Tribe"
+                },
+                "Oluwadamiloju Makinde":
+                {
+                    "firstPreference": "Data Quality Dashboards",
+                    "secondPreference": "Business Intelligence/Data Visualisation",
+                    "thirdPreference": "Turing Tribe"
+                },
+                "Roshaan Bajwa":
+                {
+                    "firstPreference": "Turing Tribe",
+                    "secondPreference": "Spine Core",
+                    "thirdPreference": "Cyber Security"
+                },
+                "Scott Caldwell-Nichols":
+                {
+                    "firstPreference": "Data Management/Engineering",
+                    "secondPreference": "NHS Pathways - Reporting Team",
+                    "thirdPreference": "Spine Core"
+                },
+                "Zahra Ahmed":
+                {
+                    "firstPreference": "NHS Pathways - Reporting Team",
+                    "secondPreference": "111 Online - Developer",
+                    "thirdPreference": "Data Management/Engineering"
+                }
+            }
+        }
+        self.simplifiedPreferencesObj = {
+            "placements": {
+                "Adult Social Care Statistics": {
+                    "directorate": "Data Services",
+                    "numberOfGrads": 1
+                },
+                "Business Intelligence/Data Visualisation": {
+                    "directorate": "Data Services",
+                    "numberOfGrads": 1
+                },
+                "Turing Tribe": {
+                    "directorate": "Data Services",
+                    "numberOfGrads": 1
+                },
+                "Spine Core": {
+                    "directorate": "Platforms",
+                    "numberOfGrads": 3
+                },
+                "111 Online - Developer": {
+                    "directorate": "Product Development",
+                    "numberOfGrads": 1
+                },
+                "Cyber Security": {
+                    "directorate": "IT Operations (Including Cyber Security)",
+                    "numberOfGrads": 1
+                },
+            },
+            "preferences": {
+                "Amaan Ibn-Nasar": {
+                    "firstPreference": "Spine Core",
+                    "secondPreference": "Adult Social Care Statistics",
+                    "thirdPreference": "111 Online - Developer"
+                },
+                "Alice Tapper": {
+                    "firstPreference": "Business Intelligence/Data Visualisation",
+                    "secondPreference": "Cyber Security",
+                    "thirdPreference": "Turing Tribe"
+                },
+            }
+        }
+
+    def test_readPreferenceFile(self):
+        self.assertEqual(self.preferencesObj,
+                         self.preferenceMatcher.readPreferenceFile())
+
+    def test_addPlacementNodesToGraph(self):
+        placements = ["Adult Social Care Statistics",
+                      "Business Intelligence/Data Visualisation", "Turing Tribe", "Spine Core_1", "Spine Core_2", "Spine Core_3",  "111 Online - Developer",  "Cyber Security"]
+        actualPlacements = self.preferenceMatcher.extractPlacementNames(
+            self.simplifiedPreferencesObj["placements"])
+        self.assertEqual(placements, actualPlacements)
+
+    def test_addPeopleNodesToGraph(self):
+        people = ["Amaan Ibn-Nasar", "Alice Tapper"]
+        actualPeople = self.preferenceMatcher.extractPeopleNames(
+            self.simplifiedPreferencesObj["preferences"])
+        self.assertEqual(people, actualPeople)
+
+    def test_convertPlacementsToGraph(self):
+        preferencesGraph = self.preferenceMatcher.convertPreferencesToGraph(
+            self.simplifiedPreferencesObj)
+        expectedEdges = [('Adult Social Care Statistics', 'Amaan Ibn-Nasar'),
+                         ('Adult Social Care Statistics', 'Alice Tapper'),
+                         ('Business Intelligence/Data Visualisation', 'Amaan Ibn-Nasar'),
+                         ('Business Intelligence/Data Visualisation', 'Alice Tapper'),
+                         ('Turing Tribe', 'Amaan Ibn-Nasar'), ('Turing Tribe',
+                                                               'Alice Tapper'),
+                         ('Spine Core_1', 'Amaan Ibn-Nasar'), ('Spine Core_1',
+                                                               'Alice Tapper'),
+                         ('Spine Core_2', 'Amaan Ibn-Nasar'), ('Spine Core_2',
+                                                               'Alice Tapper'),
+                         ('Spine Core_3', 'Amaan Ibn-Nasar'), ('Spine Core_3',
+                                                               'Alice Tapper'),
+                         ('111 Online - Developer', 'Amaan Ibn-Nasar'),
+                         ('111 Online - Developer', 'Alice Tapper'),
+                         ('Cyber Security', 'Amaan Ibn-Nasar'),
+                         ('Cyber Security', 'Alice Tapper')]
+        self.assertEqual(list(preferencesGraph.edges()), expectedEdges)
+
+    def test_applyPreferenceWeighting(self):
+        preferencesGraph = self.preferenceMatcher.convertPreferencesToGraph(
+            self.simplifiedPreferencesObj)
+        self.preferenceMatcher.applyPreferenceWeighting(
+            preferencesGraph, self.simplifiedPreferencesObj)
+        amaanWeights = [v["weight"]
+                        for _, v in preferencesGraph["Amaan Ibn-Nasar"].items()]
+        aliceWeights = [v["weight"]
+                        for _, v in preferencesGraph["Alice Tapper"].items()]
+        self.assertEqual(
+            [150, 100, 100, 175, 175, 175, 125, 100], amaanWeights)
+        self.assertEqual(
+            [100, 175, 125, 100, 100, 100, 100, 150], aliceWeights)
+
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
